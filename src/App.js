8733f2fe5ed8eea51c25db64fc13f7d4079a439f
Md Faisal Hossain <faisalh.iu@gmail.com>
2022-02-19 21:55:41 +0100

    inisial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Products, Navbar } from './components';
+
+const App = () => {
+  return (
+    <>
+      <Navbar />
+      <Products />
+    </>
+  );
+};
+
+export default App;
